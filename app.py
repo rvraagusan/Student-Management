e58972b1df4be796e8f9dafa3ee9a33abbb19ae3
rvraagusan <ravi.ragul17@gmail.com>
2021-12-08 16:57:25 +0530

    Update, find and delete DB connection

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , jsonify, request
+from flask import Flask , jsonify, request, Response
 
 from DB import db
 
@@ -9,15 +9,47 @@ app = Flask(__name__)
 @app.route('/')
 def dummy_api():
     return jsonify(data='welcome for Flask API')
-@app.route('/insert', methods=['PUT','POST'])
+@app.route('/insert_student', methods=['PUT','POST'])
 def insert_student():
     _json = request.json
     if request.method == 'PUT':
         msg = database.insert_data('STUDENT', _json)
-        return msg
+        return msg, 200
     else:
-        return jsonify(message='Insert data properly') 
+        return {'message':'Request method in invalid' }, 405
 
+@app.route('/students', methods=['GET'])
+def get_students():
+    if request.method == 'GET':
+        data = database.get_data('STUDENT')
+        return data, 200
+    else:
+        return {'message':'Request method in invalid' }, 405
+
+@app.route('/student/<id>', methods=['GET'])
+def get_student(id):
+    if request.method=='GET':
+        data = database.get_data('STUDENT', id)
+        return data, 200
+    else : 
+        return {'message':'Request method in invalid' }, 405
+
+@app.route('/student/remove/<id>', methods=['DELETE'])
+def delete_student(id):
+    if request.method=='DELETE':
+        resp = database.detele_data('STUDENT', id)
+        return resp ,200
+    else : 
+        return {'message':'Request method in invalid' }, 405
+
+@app.route('/student/update/<id>', methods=['POST'])
+def update_student(id):
+    _json = request.json
+    if request.method == 'POST':
+        msg = database.update_data('STUDENT',id , _json)
+        return msg , 200
+    else:
+        return {'message':'Request method in invalid' }, 405
 
 if __name__ == '__main__':
     app.run(debug=True)
